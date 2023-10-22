cb92d65519df5bf0a598a873f4e9759c2ea4a1d7
Tim M <timothy.romero.manas@gmail.com>
2023-10-22 11:32:08 -0400

    Adding locators to Product Page

--- page-objects/ProductsPage.js
+++ page-objects/ProductsPage.js
@@ -1,11 +1,23 @@
+import { expect } from '@playwright/test'
+
 export class ProductsPage {
-  // This is constructor method
+  // Place all PageObjects here in constructor
   constructor(page) {
     this.page = page
+    this.addButtons = page.locator('[data-qa="product-button"]')
   }
 
   // This is visit function
   visit = async () => {
     await this.page.goto('/')
+  }
+
+  async addProductToBasket(index) {
+    const specificAddButton = this.addButtons.nth(index)
+    await specificAddButton.waitFor()
+
+    await expect(specificAddButton).toHaveText('Add to Basket')
+    await specificAddButton.click()
+    await expect(specificAddButton).toHaveText('Remove from Basket')
   }
 }
--- tests/new_user_full_journey.spec.js
+++ tests/new_user_full_journey.spec.js
@@ -4,5 +4,9 @@ import { ProductsPage } from '../page-objects/ProductsPage.js'
 test.only('New user Full End to End Test', async ({ page }) => {
   const productsPage = new ProductsPage(page)
   await productsPage.visit()
-  //   await page.pause()
+  await productsPage.addProductToBasket(0)
+  await productsPage.addProductToBasket(1)
+  await productsPage.addProductToBasket(2)
+
+  // await page.pause()
 })
