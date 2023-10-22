a6b45aa2c8a6666534ab7d09000fa2d78717d2fe
Tim M <timothy.romero.manas@gmail.com>
2023-10-22 11:55:31 -0400

    Get innerText of locators

--- page-objects/ProductsPage.js
+++ page-objects/ProductsPage.js
@@ -5,6 +5,7 @@ export class ProductsPage {
   constructor(page) {
     this.page = page
     this.addButtons = page.locator('[data-qa="product-button"]')
+    this.basketCounter = page.locator('[data-qa="header-basket-count"]')
   }
 
   // This is visit function
@@ -12,12 +13,23 @@ export class ProductsPage {
     await this.page.goto('/')
   }
 
+  async getBasketCount() {
+    this.basketCounter.waitFor()
+    const text = await this.basketCounter.innerText()
+    return parseInt(text, 10)
+  }
+
   async addProductToBasket(index) {
     const specificAddButton = this.addButtons.nth(index)
     await specificAddButton.waitFor()
-
     await expect(specificAddButton).toHaveText('Add to Basket')
+    const basketCountBeforeAdding = await this.getBasketCount()
+
     await specificAddButton.click()
+
     await expect(specificAddButton).toHaveText('Remove from Basket')
+    const basketCountAfterAdding = await this.getBasketCount()
+
+    expect(basketCountAfterAdding).toBeGreaterThan(basketCountBeforeAdding)
   }
 }
