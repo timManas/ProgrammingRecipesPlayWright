5dbb7fd09ac19c3c5d7aa187953a0fcc3801990b
Tim M <timothy.romero.manas@gmail.com>
2023-10-26 21:29:23 -0400

    Create a global setup to execute before tests

--- data/userDetails.js
+++ data/userDetails.js
@@ -1,6 +1,3 @@
-import * as dotenv from 'dotenv'
-dotenv.config() // WTF ...why do we need this here ?
-
 const adminDetails = {
   username: process.env.ADMIN_USER,
   password: process.env.ADMIN_PASSWORD,
---
+++ globalSetup.js
@@ -0,0 +1,5 @@
+import * as dotenv from 'dotenv'
+
+export default () => {
+  dotenv.config() // WTF ...why do we need this here ?
+}
